23c1b60163c849dcabb9881467314d7092f11d7a
WilfredTheBoss <119538247+WilfredTheBoss@users.noreply.github.com>
2025-01-10 18:36:26 -0500

    base train class

---
+++ objects/train_car/train_car.gd
@@ -0,0 +1,27 @@
+class_name TrainCar
+extends Area3D
+
+# --- exports ---
+
+# --- node linkers ---
+@export_subgroup("Node Linkers")
+@export var collision_shape : CollisionShape3D
+@export var mesh : MeshInstance3D
+@export var connect_point : Marker3D
+
+# --- resources ---
+@export_subgroup("Resources")
+
+# --- public variables ---
+
+# --- private variables ---
+
+# --- constants ---
+
+# --- signals ---
+
+# --- method overrides ---
+
+# --- public methods ---
+
+# --- private methods ---
